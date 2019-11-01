08e75b805119132ae3b45c3e2b5d0f45e13f78d4
huy21298 <huy.821183@gmail.com>
2019-11-01 16:34:00 +0700

    ok ok

--- app/Http/Controllers/API/NguoiChoiAPI.php
+++ app/Http/Controllers/API/NguoiChoiAPI.php
@@ -8,9 +8,15 @@ use App\NguoiChoi;
 
 class NguoiChoiAPI extends Controller
 {
-    public function layDSXepHang()
+    public function layDSXepHang(Request $request)
     {
-        $db = NguoiChoi::orderBy('diem', 'desc')->limit(25)->get();
+        $currentPage = $request->query('page', 1);
+        $take = $request->query('limit', 25);
+        $skip = ($currentPage - 1);
+        $db = NguoiChoi::orderBy('diem', 'desc')
+                            ->take($take)
+                            ->skip($skip)
+                            ->get();
         $res = [
             "success"   => true,
             "data"      => $db
--- database/seeds/LinhVucSeed.php
+++ database/seeds/LinhVucSeed.php
@@ -13,28 +13,36 @@ class LinhVucSeed extends Seeder
     {
         $db = [
             [
-                "ten_linh_vuc"  => "Âm nhạc"
+                "ten_linh_vuc"  => "Âm nhạc",
+                "img"   => "https://picsum.photos/200"
             ],
             [
-                "ten_linh_vuc"  => "Vật lý"
+                "ten_linh_vuc"  => "Vật lý",
+                "img"   => "https://picsum.photos/200"
             ],
             [
-                "ten_linh_vuc"  => "Hoá học"
+                "ten_linh_vuc"  => "Hoá học",
+                "img"   => "https://picsum.photos/200"
             ],
             [
-                "ten_linh_vuc"  => "Ngữ văn"
+                "ten_linh_vuc"  => "Ngữ văn",
+                "img"   => "https://picsum.photos/200"
             ],
             [
-                "ten_linh_vuc"  => "Toán học"
+                "ten_linh_vuc"  => "Toán học",
+                "img"   => "https://picsum.photos/200"
             ],
             [
-                "ten_linh_vuc"  => "Thể dục"
+                "ten_linh_vuc"  => "Thể dục",
+                "img"   => "https://picsum.photos/200"
             ],
             [
-                "ten_linh_vuc"  => "Lịch sử"
+                "ten_linh_vuc"  => "Lịch sử",
+                "img"   => "https://picsum.photos/200"
             ],
             [
-                "ten_linh_vuc"  => "Âm nhạc"
+                "ten_linh_vuc"  => "Âm nhạc",
+                "img"   => "https://picsum.photos/200"
             ],
         ];
 
